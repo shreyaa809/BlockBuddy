be3361c42ca8ecd6fca4a5eba80954acc292da25
Shreya Shukla <shreyashukla218@gmail.com>
2026-04-05 06:23:00 +0530

    idk3

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+       <HashRouter> 
       <LanguageProvider>
         <App />
       </LanguageProvider>
-    </BrowserRouter>
+       </HashRouter> 
   </React.StrictMode>
 );
